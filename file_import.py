66782349e3095ed7b528ee7f300faccc01636767
sheltowt <sheltowt@gmail.com>
2016-07-29 09:12:21 -0400

    adding updates

--- file_import.py
+++ file_import.py
@@ -2,25 +2,33 @@ import json
 import pandas as pd
 import xmltodict
 
+# write a function that will take a string file location
+# and will return a python dictionary of the json object
+# example json_import('../files/data.json')
 def json_import(file_name):
-	with open(file_name) as json_file:
-		json_data = json.load(json_file)
-	return json_data
 
-def csv_import(file_name):
-	data_frame = pd.read_csv(file_name)
-	return data_frame
 
-def text_import(file_name):
-	f = open(file_name)
-	lines = []
-	for line in f.readlines():
-		lines.append(line)
-	return lines
 
-def xml_import(file_name):
-	with open(file_name) as file:
-		doc = xmltodict.parse(file.read())
-	return doc
+
+# csv_import takes a string file location
+# use the pandas.read_csv function 
+# to return a data frame of the loaded csv file
+#def csv_import(file_name):
+
+
+
+
+# write a function that takes a string file location of a text file
+# read in each line of the text file and add it to an array
+# return the array of lines
+#def text_import(file_name):
+
+
+
+
+# write a function that takes a string location of an xml file
+# return the xml file as a python dictionary
+#def xml_import(file_name):
+
 
 
--- file_import_test.py
+++ file_import_test.py
@@ -10,23 +10,23 @@ class TestFileImport(unittest.TestCase):
 		self.assertEqual(data["breakfast"]["food"], "cereal")
 
 	#use the pandas library read_csv method for this
-	def test_csv_import(self):
-		csv_file = "./files/data.csv"
-		data = csv_import(csv_file)
+	# def test_csv_import(self):
+	# 	csv_file = "./files/data.csv"
+	# 	data = csv_import(csv_file)
 
-		self.assertEqual(data["first_name"][3], "Eduardo")
+	# 	self.assertEqual(data["first_name"][3], "Eduardo")
 
-	def test_text_import(self):
-		txt_file = "./files/data.txt"
-		data = text_import(txt_file)
+	# def test_text_import(self):
+	# 	txt_file = "./files/data.txt"
+	# 	data = text_import(txt_file)
 
-		self.assertEqual(data, ['I really enjoy writing code\n', 'I also enjoy learning about cybersecurity'])
+	# 	self.assertEqual(data, ['I really enjoy writing code\n', 'I also enjoy learning about cybersecurity'])
 
-	def test_xml_import(self):
-		xml_file = "./files/data.xml"
-		data = xml_import(xml_file)
+	# def test_xml_import(self):
+	# 	xml_file = "./files/data.xml"
+	# 	data = xml_import(xml_file)
 
-		self.assertEqual(data['note']['to'], 'Tove')
+	# 	self.assertEqual(data['note']['to'], 'Tove')
 
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
